6de9c42ec75741f3c40db84b03ebe979a51cf972
jilonglv <jilonglv@gmail.com>
2018-05-07 20:15:18 +0800

    First Version

---
+++ Impl/JobDetailRemoteImpl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quartz.Impl
+{
+    [Serializable]
+    public class JobDetailRemoteImpl : JobDetailImpl
+    {
+        public JobDetailRemoteImpl(string name, string typename)
+            : this(name, null, typename)
+        {
+        }
+        public JobDetailRemoteImpl(string name, string group, string typename)
+        {
+            Name = name;
+            Group = group;
+            jobTypename = typename;
+        }
+        public JobDetailRemoteImpl(string name, string group, string typename, bool isDurable, bool requestsRecovery)
+        {
+            Name = name;
+            Group = group;
+            jobTypename = typename;
+            Durable = isDurable;
+            RequestsRecovery = requestsRecovery;
+        }
+        string jobTypename;
+        public string JobTypeName { get { return jobTypename; } }
+    }
+}
